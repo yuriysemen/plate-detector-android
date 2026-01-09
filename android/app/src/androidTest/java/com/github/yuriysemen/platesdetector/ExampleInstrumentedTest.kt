a84255d4f21d8d174a5effe3f46b4d6dedcf3111
Yuriy <yuriy.semen@gmail.com>
2026-01-09 11:35:00 +0200

    Add main flow instrumentation tests

--- android/app/src/androidTest/java/com/github/yuriysemen/platesdetector/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/com/github/yuriysemen/platesdetector/ExampleInstrumentedTest.kt
@@ -1,24 +1,32 @@
 package com.github.yuriysemen.platesdetector
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.yuriysemen.platesdetector", appContext.packageName)
+    fun mainFlowShowsNoModelsMessaging() {
+        composeRule.onNodeWithText("No TFLite models found").assertIsDisplayed()
+        composeRule.onNodeWithText("Retry").assertIsDisplayed()
+        composeRule.onNodeWithText(
+            "Add at least one model file",
+            substring = true
+        ).assertIsDisplayed()
+    }
+
+    @Test
+    fun retryKeepsNoModelsState() {
+        composeRule.onNodeWithText("Retry").performClick()
+        composeRule.onNodeWithText("No TFLite models found").assertIsDisplayed()
     }
 }
