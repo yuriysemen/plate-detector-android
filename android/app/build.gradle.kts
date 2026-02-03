9787c0285369c80c1bd2870258fec05382f9cf8a
Yuriy Semen <yuriy.semen@gmail.com>
2026-02-04 00:11:07 +0200

    Everything should works well

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "com.github.yuriysemen.platesdetector"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
